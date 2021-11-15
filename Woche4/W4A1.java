30d4046815f40bb1b985777bb3db6c0906073fb3
ThoreHaupt <52215971+ThoreHaupt@users.noreply.github.com>
2021-11-15 20:29:18 +0100

    W4A1

--- Woche4/W4A1.java
+++ Woche4/W4A1.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class W4A1{
 
     public static main(String[] args){
 
-        System.out.println("hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        
+        int jahre = input/(24*360*365);
+        int tage = (input/(24*360))%(24*360*365);
+        int seks = input%(24*360);
+        int stunden = seks/(24*360);
+        int minuten = seks%(24*360)/60;
+        int sekunden = seks/%(24*360);
+
+
+        System.out.println("Jahre: " + jahre);
+        System.out.println("Tage: " + tage);
+        System.out.println("Stunden: " + stunden);
+        System.out.println("Minuten: " + minuten);
+        System.out.println("Sekunden: " + sekunden);
+
+        scanner.close();
 
     }
 }
