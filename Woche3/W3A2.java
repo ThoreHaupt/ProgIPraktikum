22cfbf7873bb9abbe2a36e36d9d5e69c9eaa7f0b
ThoreHaupt <52215971+ThoreHaupt@users.noreply.github.com>
2021-11-15 20:56:35 +0100

    A2

--- Woche3/W3A2.java
+++ Woche3/W3A2.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class W3A1{
 
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("bitte oberes Limit eingeben: ")
+        int limitupper = scanner.nextInt();
+        System.put.println("bitte unteres Limit eingeben: ")
+        int limitlower = scanner.nextInt();
+
+        for (int i = 0; i<100; i++){
+            System.println("");
+        }
+
+        System.out.println("raten sie eine Zahl! ")
+        int guess = scanner.nextInt();
+
+        if (guess > limitupper || guess < limitlower){
+            System.out.println("Haha sie haben verloren");
+        }
+        if (guess < limitupper || guess > limitlower){
+            System.out.println("gz das war richtig");
+        }
+
     }
 }
