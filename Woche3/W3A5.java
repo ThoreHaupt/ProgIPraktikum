59ac4493f2a7783aad73651645885fbb8f0b41d7
ThoreHaupt <52215971+ThoreHaupt@users.noreply.github.com>
2021-11-15 22:13:13 +0100

    A5

---
+++ Woche3/W3A5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class W3A5{
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        scanner.close();
+
+        String[] words = {"Null","Eins","Zwei","Drei","Vier","Fünf","Sechs","Sieben","Acht","Neun","Zehn"}
+        
+        int i = 10;
+        while(input%i>0){
+            System.out.print(words[input%i/(i/10)])
+            i *= 10;
+        }
+    }
+}
