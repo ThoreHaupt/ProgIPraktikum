4b079511e71b8850a415a188b8db8116c036ba96
ThoreHaupt <52215971+ThoreHaupt@users.noreply.github.com>
2021-11-15 21:56:14 +0100

    A3

---
+++ Woche3/W3A3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class W3A3{
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = nextInt();
+
+        int i = 0;
+        int result = 1;
+        while(i < input){i++; restult * i }
+        Sytem.out.println(result);
+
+
+        scanner.close();
+    }
+}
