fe6da4e9d423a0be81c789b58bf5cb6dd27d73cd
ThoreHaupt <52215971+ThoreHaupt@users.noreply.github.com>
2021-11-15 20:42:57 +0100

    W4A1 funktioniert jetzt

--- Woche4/W4A1.java
+++ Woche4/W4A1.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class W4A1{
 
-    public static main(String[] args){
+    public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
 
         int input = scanner.nextInt();
-
+        System.out.println("Alle Sekunden: " + input);
         
-        int jahre = input/(24*360*365);
-        int tage = (input/(24*360))%(24*360*365);
-        int seks = input%(24*360);
-        int stunden = seks/(24*360);
-        int minuten = seks%(24*360)/60;
-        int sekunden = seks/%(24*360);
+        int jahre = input/(24*3600*365);
+        int tage = input%(24*3600*365)/(3600*24);
+        int seks = input%(24*365*3600*jahre + (24 * 3600 * tage));
+        int stunden = seks/(3600);
+        int minuten = seks%3600/60;
+        int sekunden = seks%60;
 
 
         System.out.println("Jahre: " + jahre);
